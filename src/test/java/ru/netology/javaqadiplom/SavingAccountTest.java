b3fa32fb595390750e905b64dc7218be73deb819
Anisenko Elvira <elvira.anisenko@yandex.ru>
2023-11-05 12:09:13 +0300

    Correct tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -8,53 +8,53 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class SavingAccountTest {
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/ValidData.csv")
+    @CsvFileSource(files = "src/test/resources/ValidData.csv")
     public void shouldSavingAccountWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         Assertions.assertEquals(expected, account.getBalance());
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/InvalidData.csv")
+    @CsvFileSource(files = "src/test/resources/InvalidData.csv")
     public void shouldNotSavingAccountWithInvalidData(int initialBalance, int minBalance, int maxBalance, int rate) {
         Assertions.assertThrows(IllegalArgumentException.class, () ->
                 new SavingAccount(initialBalance, minBalance, maxBalance, rate));
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/AddValidData.csv")
-    public void shouldAddWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount,int expected) {
+    @CsvFileSource(files = "src/test/resources/AddValidData.csv")
+    public void shouldAddWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         account.add(amount);
         Assertions.assertEquals(expected, account.getBalance());
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/AddInvalidData.csv")
-    public void shouldNotAddWithInvalidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount,int expected) {
+    @CsvFileSource(files = "src/test/resources/AddInvalidData.csv")
+    public void shouldNotAddWithInvalidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         account.add(amount);
         Assertions.assertEquals(expected, account.getBalance());
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/PayValidData.csv")
-    public void shouldPayWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount,int expected) {
+    @CsvFileSource(files = "src/test/resources/PayValidData.csv")
+    public void shouldPayWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         account.pay(amount);
         Assertions.assertEquals(expected, account.getBalance());
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/PayInvalidData.csv")
-    public void shouldNotPayWithInvalidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount,int expected) {
+    @CsvFileSource(files = "src/test/resources/PayInvalidData.csv")
+    public void shouldNotPayWithInvalidData(int initialBalance, int minBalance, int maxBalance, int rate, int amount, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         account.pay(amount);
         Assertions.assertEquals(expected, account.getBalance());
     }
 
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/YearChangeData.csv")
+    @CsvFileSource(files = "src/test/resources/YearChangeData.csv")
     public void shouldYearChangeWithValidData(int initialBalance, int minBalance, int maxBalance, int rate, int expected) {
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
         Assertions.assertEquals(expected, account.yearChange());
